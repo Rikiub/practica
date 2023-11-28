fc36d273d7179ff7700b6ac3293ca6510a4d28f8
Rikiub <rikiub.dev@gmail.com>
2023-11-28 15:05:04 -0400

    update

--- cine.sql
+++ cine.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-11-2023 a las 17:59:49
--- Versión del servidor: 10.1.37-MariaDB
--- Versión de PHP: 5.6.39
+-- Tiempo de generación: 28-11-2023 a las 20:02:10
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `actores`
+--
+
+CREATE TABLE `actores` (
+  `codigo` int(10) NOT NULL,
+  `nombre_completo` varchar(30) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `sexo` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `elenco`
 --
 
@@ -32,7 +44,7 @@ CREATE TABLE `elenco` (
   `cod_pelicula` int(11) NOT NULL,
   `cod_actor` varchar(10) NOT NULL,
   `papel` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -45,7 +57,7 @@ CREATE TABLE `peliculas` (
   `titulo` varchar(30) NOT NULL,
   `fecha_estreno` date NOT NULL,
   `genero` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `peliculas`
@@ -60,6 +72,18 @@ INSERT INTO `peliculas` (`codigo`, `titulo`, `fecha_estreno`, `genero`) VALUES
 --
 
 --
+-- Indices de la tabla `actores`
+--
+ALTER TABLE `actores`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Indices de la tabla `elenco`
+--
+ALTER TABLE `elenco`
+  ADD PRIMARY KEY (`cod_pelicula`);
+
+--
 -- Indices de la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
@@ -70,10 +94,32 @@ ALTER TABLE `peliculas`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `actores`
+--
+ALTER TABLE `actores`
+  MODIFY `codigo` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
   MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `actores`
+--
+ALTER TABLE `actores`
+  ADD CONSTRAINT `actores_ibfk_1` FOREIGN KEY (`codigo`) REFERENCES `peliculas` (`codigo`);
+
+--
+-- Filtros para la tabla `elenco`
+--
+ALTER TABLE `elenco`
+  ADD CONSTRAINT `elenco_ibfk_1` FOREIGN KEY (`cod_pelicula`) REFERENCES `peliculas` (`codigo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
